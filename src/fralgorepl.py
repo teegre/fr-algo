e34aa06652698015e0d2bf10f05b10552d98807e
Stéphane MEYER <tigerlost@me.com>
2024-03-25 21:30:11 +0100

    Added readline, a logo and a prompt in the REPL.

--- src/fralgorepl.py
+++ src/fralgorepl.py
@@ -1,18 +1,30 @@
 #! /usr/bin/env python
 import sys
+import readline
 from algoparser import parser, reset
 
 if __name__ == '__main__':
+  readline.parse_and_bind('"[" "\C-v[]\e[D"')
+  readline.parse_and_bind('"(" "\C-v()\e[D"')
+  print("  __           _             ")
+  print(" / _|_ __ __ _| | __ _  ___  ")
+  print("| |_| '__/ _` | |/ _` |/ _ \ ")
+  print("|  _| | | (_| | | (_| | (_) |")
+  print("|_| |_|  \__,_|_|\__, |\___/ ")
+  print("ALGORITHMES      |___/ 500mg ")
+  print()
+  print('CTRL+d pour quitter.')
+
   while True:
     try:
-      instruction = input('ALGO> ')
+      instruction = input('::> ')
       if instruction:
         parser.parse(instruction + '\n').eval()
       else:
         continue
     except EOFError:
       print()
-      print('Au revoir !')
+      print('*** Au revoir !')
       sys.exit(0)
     except KeyboardInterrupt:
       reset()

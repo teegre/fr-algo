4f3f1a191ba7477c4552f4e24611b8b50fd23fa6
Stéphane MEYER <tigerlost@me.com>
2024-04-12 00:10:44 +0200

    Structure: type check (3)

--- fralgo/lib/ast.py
+++ fralgo/lib/ast.py
@@ -26,7 +26,7 @@ import operator
 from random import random
 from fralgo.lib.datatypes import map_type
 from fralgo.lib.datatypes import Array, Boolean, Number, Float, Integer, String
-from fralgo.lib.datatypes import Structure
+from fralgo.lib.datatypes import Structure, is_structure
 from fralgo.lib.symbols import declare_array, declare_sized_char, declare_var, declare_structure
 from fralgo.lib.symbols import get_variable, assign_value
 from fralgo.lib.file import new_file_descriptor, get_file_descriptor, clear_file_descriptor
@@ -110,8 +110,7 @@ class DeclareStruct:
   def eval(self):
     for field, datatype in self.fields:
       if datatype not in self.__types:
-        if isinstance(datatype, tuple):
-          # sized Caractère type.
+        if isinstance(datatype, tuple) or is_structure(datatype):
           continue
         raise BadType(f'Type invalide : {self.name}.{field} en >{datatype}<')
     declare_structure(Structure(self.name, self.fields))

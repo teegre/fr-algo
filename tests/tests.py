0ba1bb468813b71a5bdc4bbf51fb9f561b1e002b
Stéphane MEYER <tigerlost@me.com>
2024-10-04 17:42:36 +0200

    Removed useless output.

--- tests/tests.py
+++ tests/tests.py
@@ -38,7 +38,6 @@ class Test(unittest.TestCase):
         s.b[i] <- s.b[i] + 1
       i Suivant
       test <- test ET s.b[0] = 2 ET s.b[5] = 7
-      Ecrire test
     Fin'''
 
     reset_parser()
@@ -46,7 +45,6 @@ class Test(unittest.TestCase):
     statements.eval()
     t = sym.get_variable('test')
     self.assertEqual(t.eval(), True, 'test should be VRAI')
-    print()
 
   def test_tableau_direct(self):
     prog='''Tableau t[8] en Entier
@@ -56,7 +54,6 @@ class Test(unittest.TestCase):
       t[] <- 1, 2, 3, 4, 5, 6, 7, 8, 9
       test1 <- t[0] = 1
       test2 <- t[8] = 9
-      Ecrire test1, test2
     Fin'''
 
     reset_parser()
@@ -66,7 +63,6 @@ class Test(unittest.TestCase):
     t2 = sym.get_variable('test2')
     self.assertEqual(t1.eval(), True, 'test1 should be VRAI')
     self.assertEqual(t2.eval(), True, 'test2 should be VRAI')
-    print()
 
   def test_table(self):
     prog='''Table t
@@ -82,7 +78,6 @@ class Test(unittest.TestCase):
       t[clef] <- valeur
       test1 <- t[clef] = valeur
       test2 <- NON(Existe(t, 0))
-      Ecrire test1, test2
     Fin'''
 
     reset_parser()
@@ -92,7 +87,6 @@ class Test(unittest.TestCase):
     t2 = sym.get_variable('test2')
     self.assertEqual(t1.eval(), True, 'test1 should be VRAI')
     self.assertEqual(t2.eval(), True, 'test2 should be VRAI')
-    print()
 
   def test_procedure_split(self):
     prog = '''Procédure scinder(chaine, separateur, &elements[] en Chaîne)
@@ -117,7 +111,6 @@ class Test(unittest.TestCase):
       scinder("TEST=REUSSI", "=", elements)
       test1 <- elements[0] = "TEST"
       test2 <- elements[1] = "REUSSI"
-      Ecrire test1, test2
     Fin'''
 
     reset_parser()
@@ -127,7 +120,6 @@ class Test(unittest.TestCase):
     t2 = sym.get_variable('test2')
     self.assertEqual(t1.eval(), True, 'test1 should be VRAI')
     self.assertEqual(t2.eval(), True, 'test2 should be VRAI')
-    print()
 
   def test_structure_in_resized_array(self):
     prog = '''Structure S
@@ -147,7 +139,6 @@ class Test(unittest.TestCase):
       v Suivant
       test1 <- t[0].a = 1 ET t[0].b = 2
       test2 <- t[n].a = 9 ET t[n].b = 10
-      Ecrire test1, test2
     Fin'''
 
     reset_parser()
@@ -157,7 +148,6 @@ class Test(unittest.TestCase):
     t2 = sym.get_variable('test2')
     self.assertEqual(t1.eval(), True, 'test1 should be VRAI')
     self.assertEqual(t2.eval(), True, 'test2 should be VRAI')
-    print()
 
   def test_function(self):
     prog = '''Fonction somme_chaine(n1, n2 en Entier) en Chaîne
@@ -175,7 +165,6 @@ class Test(unittest.TestCase):
       resultat <- somme_chaine(x1, x2)
       attendu <- Chaîne(x1) & " + " & Chaîne(x2) & " = " & Chaîne(x1 + x2)
       test <- resultat = attendu
-      Ecrire test
     Fin'''
 
     reset_parser()
@@ -183,7 +172,6 @@ class Test(unittest.TestCase):
     statements.eval()
     t = sym.get_variable('test')
     self.assertEqual(t.eval(), True, 'test should be VRAI')
-    print()
 
   def test_structure(self):
     prog = '''Structure P
@@ -201,7 +189,6 @@ class Test(unittest.TestCase):
       test2 ← p1.a = 3 ET p1.b = 4
       p2 <- p1
       test3 <- p2 = p1
-      Ecrire test1, test2, test3
     Fin'''
 
     reset_parser()
@@ -213,7 +200,6 @@ class Test(unittest.TestCase):
     self.assertEqual(t2.eval(), True, 'test2 should be VRAI')
     t3 = sym.get_variable('test3')
     self.assertEqual(t3.eval(), True, 'test3 should be VRAI')
-    print()
 
   def test_array_declaration(self):
     prog = '''Tableau T[] en Entier
@@ -227,7 +213,6 @@ class Test(unittest.TestCase):
     t = sym.get_variable('T')
     self.assertEqual(t.data_type[0], 'Tableau', 'should be Tableau')
     self.assertEqual(type(t), Array, 'should be Array')
-    print()
 
   def test_multidimensional_array_declaration(self):
     prog = '''Tableau T[2, 2] en Entier
@@ -242,7 +227,6 @@ class Test(unittest.TestCase):
     self.assertEqual(t.data_type, ('Tableau', 'Entier', (2, 2)), 'should be Tableau')
     self.assertEqual(t.sizes, (3,3), 'should be (3,3)')
     self.assertEqual(type(t), Array, 'should be Array')
-    print()
 
   def test_arrays_declaration(self):
     prog = '''Tableaux T1[], T2[2, 2], T3[3] en Entier
@@ -262,7 +246,6 @@ class Test(unittest.TestCase):
     self.assertEqual(t1.sizes, (0,), 'size should be 0')
     self.assertEqual(t2.sizes, (3,3), 'size should be 3,3')
     self.assertEqual(t3.sizes, (4,), 'size should be 4')
-    print()
 
   def test_simple_array(self):
     prog = '''Tableau T[7] en Entier
@@ -272,11 +255,6 @@ class Test(unittest.TestCase):
       Pour I ← 0 à 7
         T[I] ← I + 1
       I Suivant
-      Si T[7] = 8 Alors
-        Ecrire "T[7] est bien égal à 8 !"
-      Sinon
-        Ecrire "T[7] = 8 est", T[7] = 8
-      FinSi
     Fin'''
 
     reset_parser()
@@ -291,7 +269,6 @@ class Test(unittest.TestCase):
     self.assertEqual(t.value[5], 6, 'Should be 6')
     self.assertEqual(t.value[6], 7, 'Should be 7')
     self.assertEqual(t.value[7], 8, 'Should be 8')
-    print()
 
   def test_while(self):
     prog = '''Variable Marche en Booléen
@@ -299,10 +276,8 @@ class Test(unittest.TestCase):
       Marche ← VRAI
       Ecrire "6. Test de la boucle 'TantQue Marche'"
       TantQue Marche
-        Ecrire Marche
         Marche ← FAUX
       FinTantQue
-      Ecrire Marche
     Fin'''
 
     reset_parser()
@@ -310,7 +285,6 @@ class Test(unittest.TestCase):
     statements.eval()
     m = sym.get_variable('Marche')
     self.assertEqual(m.eval(), False, 'should be False')
-    print()
 
   def test_while_multable(self):
     prog = '''Variable N en Entier
@@ -318,7 +292,6 @@ class Test(unittest.TestCase):
       N ← 1
       Ecrire "7. Test de la boucle 'TantQue N < 11'"
       TantQue N < 11
-        Ecrire N, "x 9 =", N * 9
         N ← N + 1
       FinTantQue
     Fin'''
@@ -328,14 +301,13 @@ class Test(unittest.TestCase):
     statements.eval()
     n = sym.get_variable('N')
     self.assertEqual(n.eval(), 11, 'should be 11')
-    print()
 
   def test_for_loop_normal(self):
-    prog = '''Variable I en Entier
+    prog = '''Variables I, J en Entier
     Début
       Ecrire "8. Test de la boucle 'Pour I <- 1 à 10'"
       Pour I ← 1 à 10
-        Ecrire I
+        J <- I
       I Suivant
     Fin'''
 
@@ -343,15 +315,16 @@ class Test(unittest.TestCase):
     statements = parser.parse(prog)
     statements.eval()
     i = sym.get_variable('I')
+    j = sym.get_variable('J')
     self.assertEqual(i.eval(), 11, 'I should be 11')
-    print()
+    self.assertEqual(j.eval(), 10, 'J should be 10')
 
   def test_for_loop_reverse(self):
-    prog = '''Variable I en Entier
+    prog = '''Variables I, J en Entier
     Début
       Ecrire "9. Test de la boucle 'Pour I <- 10 à 0 Pas -1'"
       Pour I ← 10 à 0 Pas -1
-        Ecrire I
+        J <- I
       I Suivant
     Fin'''
 
@@ -359,15 +332,16 @@ class Test(unittest.TestCase):
     statements = parser.parse(prog)
     statements.eval()
     i = sym.get_variable('I')
+    j = sym.get_variable('J')
     self.assertEqual(i.eval(), -1, 'I should be -1')
-    print()
+    self.assertEqual(j.eval(), 0, 'J should be 0')
 
   def test_for_step_two(self):
-    prog = '''Variable I en Entier
+    prog = '''Variables I, J en Entier
     Début
       Ecrire "10. Test de la boucle 'Pour I <- 1 à 10 Pas 2'"
       Pour I ← 1 à 10 Pas 2
-        Ecrire I
+        J <- I
       I Suivant
     Fin'''
 
@@ -375,15 +349,16 @@ class Test(unittest.TestCase):
     statements = parser.parse(prog)
     statements.eval()
     i = sym.get_variable('I')
+    j = sym.get_variable('J')
     self.assertEqual(i.eval(), 11, 'I should be 11')
-    print()
+    self.assertEqual(j.eval(), 9, 'I should be 9')
 
   def test_for_step_negative_two(self):
-    prog = '''Variable I en Entier
+    prog = '''Variables I, J en Entier
     Début
       Ecrire "11. Test de la boucle 'Pour I <- 0 à -10 Pas -2'"
       Pour I ← 0 à -10 Pas -2
-        Ecrire I
+        J <- I
       I Suivant
     Fin'''
 
@@ -391,15 +366,17 @@ class Test(unittest.TestCase):
     statements = parser.parse(prog)
     statements.eval()
     i = sym.get_variable('I')
+    j = sym.get_variable('J')
     self.assertEqual(i.eval(), -12, 'I should be -12')
-    print()
+    self.assertEqual(j.eval(), -10, 'I should be -10')
 
   def test_for_negative(self):
-    prog = '''Variable I en Entier
+    prog = '''Variables I, J en Entier
     Début
+      J <- -1
       Ecrire "12. Test de la boucle 'Pour I <- 0 à -10'"
       Pour I ← 0 à -10
-        Ecrire I
+        J <- I
       I Suivant
     Fin'''
 
@@ -407,8 +384,9 @@ class Test(unittest.TestCase):
     statements = parser.parse(prog)
     statements.eval()
     i = sym.get_variable('I')
+    j = sym.get_variable('J')
     self.assertEqual(i.eval(), 0, 'I should be 0')
-    print()
+    self.assertEqual(j.eval(), -1, 'I should be -1')
 
   def test_if_not(self):
     prog = '''Variables A, B en Booléen
@@ -427,7 +405,6 @@ class Test(unittest.TestCase):
     statements.eval()
     b = sym.get_variable('B')
     self.assertEqual(b.eval(), False, 'I should be False')
-    print()
 
 if __name__ == '__main__':
   unittest.main()

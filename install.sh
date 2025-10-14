58dd99161f9e3038c24101c0b99665ce3211ab58
Stéphane MEYER <tigerlost@me.com>
2025-10-14 16:28:08 +0200

    Inclusion de la librairie standard au dépot.

---
+++ install.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+version="$(python -c 'from fralgo import __version__; print(__version__.replace(".beta.", "b"))')"
+[[ -z $version ]] && {
+  echo "Impossible de déterminer la version de FR-ALGO..."
+  exit 1
+}
+
+echo "o FR-ALGO version ${version}"
+echo "* Construction..."
+if python -m build &> /dev/null; then
+  command -v fralgo &> /dev/null && {
+    echo "* Désinstallation de la version précédente..."
+    pipx uninstall fralgo &> /dev/null
+  }
+  echo "* Installation de FR-ALGO..."
+  if pipx install "dist/fralgo-${version}.tar.gz" &> /dev/null; then
+    echo "* Installation de la librairie standard"
+    mkdir -p ~/.local/lib/fralgo 
+    cp ./fralgo-std/* ~/.local/lib/fralgo/ || stdlib=1
+    [[ $stdlib ]] && echo "x L'installation de la librairie standard a échoué."
+    echo "o FR-ALGO installé avec succès."
+  else
+    echo "x L'installation a échoué."
+    exit 1
+  fi
+else
+  echo "x La construction a échoué."
+  exit 1
+fi
